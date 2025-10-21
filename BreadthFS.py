6710e1ad08f3a977ebfceb4817b0bb995798c13f
Hanineyasser <es-haneen.yasser2027@alexu.edu.eg>
2025-10-21 21:57:38 +0300

    commented the BFS

--- BreadthFS.py
+++ BreadthFS.py
@@ -3,8 +3,15 @@
 from collections import deque
 from helperFuncs import neighbors
 def Bfs(initial_state, goal_state):
+    # BFS-->queue data structure
+    # queue takes--> (state, path to arrive to this state)
     queue = deque([(initial_state, [initial_state])])
     visited = set()
+    # add the initial state to the visited set
+    # initial_state-->2D array can't be added to a set directly
+    # tuple-->immutable, ordered collection-->can't change its contents
+    # we convert each row to a tuple, then the whole state to a tuple of tuples
+    # result-->getting a hashable representation of the state, that can be added to a set
     visited.add(tuple(map(tuple, initial_state)))
     while queue:
         current_state, path = queue.popleft()
@@ -14,5 +21,6 @@ def Bfs(initial_state, goal_state):
             neighbor_tuple = tuple(map(tuple, neighbor))
             if neighbor_tuple not in visited:
                 visited.add(neighbor_tuple)
+                # path + the neighbor's state-->path from start to neighbor
                 queue.append((neighbor, path + [neighbor]))
     return None
